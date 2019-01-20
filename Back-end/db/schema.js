7ae55c0a0e4e60f915eaa5304694f74e56b2b32e
Lhumeur <ivan.strekha@gmail.com>
2019-01-20 05:56:34 +0300

    Update schema.js
    - changed the structure of the database

--- Back-end/db/schema.js
+++ Back-end/db/schema.js
@@ -1,45 +1,16 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var SingersSchema = new Schema(
-  {
-    singer: {type: String, require: true}
-  }, {
-    versionKey: false
-  });
-
-var GenresSchema = new Schema(
-  {
-    genre: {type: String, require: true}
-  }, {
-    versionKey: false
-  });
-
-var YearsSchema = new Schema(
-  {
-    year: {type: Number, require: true}
-  }, {
-    versionKey: false
-  });
-
 var SongsSchema = new Schema(
   {
-    singer: {},
+    singer: {type: String},
     title: {type: String, require: true},
-    genre: {},
-    year: {}
+    genre: {type: String},
+    year: {type: Number}
   }, {
     versionKey: false
   });
 
-var Singer = mongoose.model('Singer', SingersSchema);
-var Genre = mongoose.model('Genre', GenresSchema);
-var Song = mongoose.model('Song', SongsSchema);
-var Year = mongoose.model('Year', YearsSchema);
+var ListOfSongs = mongoose.model('ListOfSong', SongsSchema);
 
-module.exports = {
-  Singer,
-  Genre,
-  Song,
-  Year
-};
+module.exports = ListOfSongs;
