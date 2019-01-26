fe60700002770c39fefa14bb7be9200f85424d69
Lhumeur <ivan.strekha@gmail.com>
2019-01-26 20:58:20 +0300

    Configured response header

--- Back-end/app.js
+++ Back-end/app.js
@@ -20,6 +20,13 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use('/', index);
 app.use('/api', api);
 
